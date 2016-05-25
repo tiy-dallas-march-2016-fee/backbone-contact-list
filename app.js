8ead60a51857cae152eaf0ba808d0fdd59aab366
Eric Sowell <eric.sowell@gmail.com>
2016-05-25 07:41:18 -0500

    added starter template and data

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ if (window.ContactList === undefined) { window.ContactList = {}; }
 
 (function(context) {
 
+  var list = [
+    new context.ContactItem({ firstName: 'Bob', lastName: 'Smith', email: 'bob@example.com', gender: 'M' }),
+    new context.ContactItem({ firstName: 'George', lastName: 'Jetson', email: 'george@thefuture.com', gender: 'M' }),
+    new context.ContactItem({ firstName: 'Kitiara', lastName: 'Uth Matar', email: 'fighter@dragonlance.com', gender: 'F' }),
+    new context.ContactItem({ firstName: 'Diana', lastName: '(of) Themyscira', email: 'lady@theamazons.com', gender: 'F' }),
+  ];
+
+  //TODO: use this data
+
+
   console.log('context', context);
 
 })(window.ContactList)
